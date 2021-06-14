cce844f426123cea3795d22fb30fd15472e133d6
vbliakhar <vovabliahar@gmail.com>
2021-06-14 21:39:31 +0200

    working on the ascent

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import './sass/main.scss';
 import NewApiService from './partials/new-service.js';
 import articleTpl from './partials/templateImg.hbs';
-import { error } from '@pnotify/core';
+import { success, error } from '@pnotify/core';
 import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/core/dist/BrightTheme.css';
 
 // const basicLightbox = require('basiclightbox');
-onFetchError();
 const refs = {
   searchForm: document.querySelector('.search-form'),
   articlesContainer: document.querySelector('.gallery'),
   loadMoreBtn: document.querySelector('.my-element-selector'),
 };
+let onMyIndex;
 const newApiService = new NewApiService();
 refs.searchForm.addEventListener('submit', onSearch);
 refs.loadMoreBtn.addEventListener('click', onLoadMore);
@@ -21,6 +21,7 @@ function onSearch(e) {
   e.preventDefault();
   newApiService.query = e.currentTarget.elements.query.value;
   if (newApiService.query.length === 0) {
+    onFetchError();
     return;
   }
   newApiService.resetPage();
@@ -36,10 +37,13 @@ function appendArticleMarkup(data) {
   // console.log(data[4].largeImageURL);
   console.log(data.length);
   if (data.length < 8) {
+    onFetchError();
     clearArticlesContainer();
     return;
   }
+  onFetchSuccess();
   refs.articlesContainer.insertAdjacentHTML('beforeend', articleTpl(data));
+  refs.articlesContainer.addEventListener('click', myImageBig);
   refs.loadMoreBtn.scrollIntoView({
     block: 'end',
     behavior: 'smooth',
@@ -48,10 +52,24 @@ function appendArticleMarkup(data) {
 function clearArticlesContainer() {
   refs.articlesContainer.innerHTML = '';
 }
+function myImageBig(data) {
+  data.preventDefault();
+  if (data.target.nodeName !== 'IMG') {
+    return;
+  }
+  console.log(data.target);
+}
+function onFetchSuccess() {
+  success({
+    title: 'Вітаю!',
+    text: 'Запит виконаний успішно',
+    delay: 4000,
+  });
+}
 function onFetchError() {
   error({
-    title: 'Oh No!',
-    text: 'Too many matches found. Please enter a more specific query!',
+    title: 'Помилка!',
+    text: 'Щось пішло не так, перевірте правильність написання!',
     delay: 4000,
   });
 }
